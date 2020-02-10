525445043b5633e838cb4e939e023571818ef61e
Lucas Barbosa Fonseca <lufonsecabarbosa@hotmail.com>
2020-02-10 15:12:29 -0300

    solução temporária de insert de matriz

--- class/classAlternatives.php
+++ class/classAlternatives.php
@@ -8,40 +8,45 @@
 
         public function insertQuestAlt($params = array()) {
 
-            /*$sql_insert = "INSERT INTO question_alternative QUESTION_NUMBER, DESC_QUESTION_ALT, ALTERNATIVE_A, ALTERNATIVE_B,
-                          ALTERNATIVE_C, ALTERNATIVE_D, ALTERNATIVE_E, ALTERNATIVE_F, ALTERNATIVE_G, ALT_CORRECT VALUES 
+            //echo '<pre>'.print_r($params).'</pre>';
+
+            $sql_insert = "INSERT INTO question_alternative (QUESTION_NUMBER, DESC_QUESTION_ALT, ALTERNATIVE_A, ALTERNATIVE_B,
+                          ALTERNATIVE_C, ALTERNATIVE_D, ALTERNATIVE_E, ALTERNATIVE_F, ALTERNATIVE_G, ALT_CORRECT) VALUES 
                           (:num_question_alt, :question_alt, :alternativeA, :alternativeB, :alternativeC, :alternativeD, 
                           :alternativeE, :alternativeF, :alternativeG, :correctAlt)";
 
-            $stmt->execute(array(':num_question_alt' => $num_question_alt, ':question_alt' => $question_alt, ':alternativeA' => $alternativeA,
-                                 ':alternativeB' => $alternativeB, ':alternativeC' => $alternativeC, ':alternativeD' => $alternativeD,
-                                 ':alternativeE' => $alternativeE, ':alternativeF' => $alternativeF, ':alternativeG' => $alternativeG,
-                                 ':correctAlt' => $correctAlt));
+            $stmt = Connection::prepare($sql_insert); 
 
-            $stmt = Connection::prepare($sql_insert);*/
+            $stmt->bindParam(':num_question_alt', $num_question_alt);
+            $stmt->bindParam(':question_alt', $question_alt);
+            $stmt->bindParam(':alternativeA', $alternativeA);
+            $stmt->bindParam(':alternativeB', $alternativeB);
+            $stmt->bindParam(':alternativeC', $alternativeC);
+            $stmt->bindParam(':alternativeD', $alternativeD);
+            $stmt->bindParam(':alternativeE', $alternativeE);
+            $stmt->bindParam(':alternativeF', $alternativeF);
+            $stmt->bindParam(':alternativeG', $alternativeG);
+            $stmt->bindParam(':correctAlt', $correctAlt); 
 
-            foreach ($params as $key => $value) {
+            foreach ($params as $nameField => $valueField) { 
+                foreach ($valueField as $indice => $value) {
+    
+                $num_question_alt = $params['num_question_alt'][$indice];
+                $question_alt = $params['question_alt'][$indice];
+                $alternativeA = $params['alternativaA'][$indice];
+                $alternativeB = $params['alternativaB'][$indice];
+                $alternativeC = $params['alternativaC'][$indice];
+                $alternativeD = $params['alternativaD'][$indice];
+                $alternativeE = $params['alternativaE'][$indice];
+                $alternativeF = $params['alternativaF'][$indice];
+                $alternativeG = $params['alternativaG'][$indice];
+                $correctAlt = $params['alt_correta'][$indice];
 
-                $num_question_alt = $params[$key]['num_question_alt'];
-                $question_alt = $params[$key]['question_alt'];
-                $alternativeA = $params[$key]['alternativaA'];
-                $alternativeB = $params[$key]['alternativaB'];
-                $alternativeC = $params[$key]['alternativaC'];
-                $alternativeD = $params[$key]['alternativaD'];
-                $alternativeE = $params[$key]['alternativaE'];
-                $alternativeF = $params[$key]['alternativaF'];
-                $alternativeG = $params[$key]['alternativaG'];
-                $correctAlt = $params[$key]['alt_correta'];
+                $stmt->execute();
 
-                $array = array(':num_question_alt' => $num_question_alt, ':question_alt' => $question_alt, ':alternativeA' => $alternativeA,
-                                ':alternativeB' => $alternativeB, ':alternativeC' => $alternativeC, ':alternativeD' => $alternativeD,
-                                ':alternativeE' => $alternativeE, ':alternativeF' => $alternativeF, ':alternativeG' => $alternativeG,
-                                ':correctAlt' => $correctAlt);
-
-                                print_r($array);
-
+                }
                 
-            }
+            } 
 
         }
 
